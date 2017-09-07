f74743504b59030a55d349362bdee13301c847b7
olamothe <olamothe@coveo.com>
2017-09-07 15:22:53 -0400

    Modified the diagnostic provider to restrict to HTML/visualforce pages

--- src/provider/diagnosticProvider.ts
+++ src/provider/diagnosticProvider.ts
@@ -3,6 +3,8 @@ import { ReferenceDocumentation } from '../referenceDocumentation';
 import { OptionsDiagnostics } from '../diagnostics/optionsDiagnostics';
 import { ResultTemplatesDiagnostics } from '../diagnostics/resultTemplatesDiagnostics';
 
+export const canProvideDiagnosticFor = ['html', 'visualforce'];
+
 export class DiagnosticProvider {
   constructor(
     public diagnosticCollection: vscode.DiagnosticCollection,
@@ -11,11 +13,13 @@ export class DiagnosticProvider {
 
   public updateDiagnostics(document: vscode.TextDocument) {
     this.diagnosticCollection.clear();
-    let allDiagnostics: vscode.Diagnostic[] = [];
-    allDiagnostics = allDiagnostics.concat(
-      new OptionsDiagnostics(this.referenceDocumentation).provideDiagnostics(document)
-    );
-    allDiagnostics = allDiagnostics.concat(new ResultTemplatesDiagnostics().provideDiagnostics(document));
-    this.diagnosticCollection.set(document.uri, allDiagnostics);
+    if (canProvideDiagnosticFor.indexOf(document.languageId.toLowerCase()) != -1) {
+      let allDiagnostics: vscode.Diagnostic[] = [];
+      allDiagnostics = allDiagnostics.concat(
+        new OptionsDiagnostics(this.referenceDocumentation).provideDiagnostics(document)
+      );
+      allDiagnostics = allDiagnostics.concat(new ResultTemplatesDiagnostics().provideDiagnostics(document));
+      this.diagnosticCollection.set(document.uri, allDiagnostics);
+    }
   }
 }
